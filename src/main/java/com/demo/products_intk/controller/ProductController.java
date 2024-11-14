5a568f445b1df3354a2e0b6a469b29af1b9406ee
kkpalma <85182515+chanchobrujo@users.noreply.github.com>
2024-11-13 19:56:10 -0500

    FIX: update utils.

--- src/main/java/com/demo/products_intk/controller/ProductController.java
+++ src/main/java/com/demo/products_intk/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.demo.products_intk.model.dto.ProductDto;
 import com.demo.products_intk.service.IProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,6 +20,7 @@ public class ProductController {
     private final IProductService productService;
 
     @GetMapping("find-by-id")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
     public ResponseEntity<Mono<ProductDto>> findById(@RequestParam String id) {
         return ok(this.productService.findById(id));
     }
--- src/main/java/com/demo/products_intk/document/ProductDocument.java
+++ src/main/java/com/demo/products_intk/document/ProductDocument.java
@@ -4,13 +4,13 @@ import com.demo.products_intk.model.dto.ProductDto;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 
-import static com.stater.intk.util.GeneralUtil.generateId;
-import static com.stater.intk.util.MapperUtils.objectToObject;
+import static com.stater.intk.common.utils.GeneralUtils.generateId;
+import static com.stater.intk.common.utils.MapperUtils.objectToObject;
 
 @Data
 public class ProductDocument {
     @Id
-    private String id = generateId(25);
+    private String id = generateId(25, false);
     private String name;
     private String type;
 
